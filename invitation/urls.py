9979103acd8b91bf606c077bf00aaa88c3179533
Nick Lang <nick@nicklang.com>
2013-05-31 21:40:09 -0600

    making django 1.5 changes

--- invitation/urls.py
+++ invitation/urls.py
@@ -1,31 +1,42 @@
 from django.conf.urls.defaults import *
-from django.views.generic.simple import direct_to_template
+from django.views.generic import TemplateView
+from django.views.generic.base import RedirectView
 from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
 from app_settings import INVITE_ONLY
 
+
+class LoginRequired(object):
+    """
+    Mixin for requiring login to a generic view.
+
+    """
+    @method_decorator(login_required)
+    def dispatch(self, *args, **kwargs):
+        return super(LoginRequired, self).dispatch(*args, **kwargs)
+
+
+class LoginRequiredTemplateView(TemplateView, LoginRequired):
+    pass
 
 login_required_direct_to_template = login_required(direct_to_template)
 
 
 urlpatterns = patterns('',
     url(r'^invitation/$',
-        login_required_direct_to_template,
-        {'template': 'invitation/invitation_home.html'},
+        LoginRequiredTemplateView.as_view(template_name="invitation/invitation_home.html"),
         name='invitation_home'),
     url(r'^invitation/invite/$',
         'invitation.views.invite',
         name='invitation_invite'),
     url(r'^invitation/invite/complete/$',
-        login_required_direct_to_template,
-        {'template': 'invitation/invitation_complete.html'},
+        LoginRequiredTemplateView.as_view(template_name="invitation/invitation_complete.html"),
         name='invitation_complete'),
     url(r'^invitation/invite/unavailable/$',
-        login_required_direct_to_template,
-        {'template': 'invitation/invitation_unavailable.html'},
+        LoginRequiredTemplateView.as_view(template_name="invitation/invitation_unavailable.html"),
         name='invitation_unavailable'),
     url(r'^invitation/accept/complete/$',
-        direct_to_template,
-        {'template': 'invitation/invitation_registered.html'},
+        TemplateView.as_view(template_name="invitation/invitation_registered.html"),
         name='invitation_registered'),
     url(r'^invitation/accept/(?P<invitation_key>\w+)/$',
         'invitation.views.register',
@@ -36,12 +47,10 @@ urlpatterns = patterns('',
 if INVITE_ONLY:
     urlpatterns += patterns('',
         url(r'^register/$',
-            'django.views.generic.simple.redirect_to',
-            {'url': '../invitation/invite_only/', 'permanent': False},
+            RedirectView.as_view(url='../invitation/invite_only/', permanent=False),
             name='registration_register'),
         url(r'^invitation/invite_only/$',
-            direct_to_template,
-            {'template': 'invitation/invite_only.html'},
+            TemplateView.as_view(template_name="invitation/invite_only.html"),
             name='invitation_invite_only'),
         url(r'^invitation/reward/$',
             'invitation.views.reward',
